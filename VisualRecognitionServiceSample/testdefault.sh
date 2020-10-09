5589169bca76d58bea90766b703cf0d2b4b7b94d
Jigar Pandya <jigarpandya.ce@gmail.com>
2020-10-10 00:24:39 +0530

    Dry run of VisualRecognition HelloWorld service.
    Simply followed "Getting started with Visual Recognition" example. Noted that when run using default the image was classified properly as motherboard relevant but when used food mode image was shown with score 1 (100%) nonfood.

---
+++ VisualRecognitionServiceSample/testdefault.sh
@@ -0,0 +1,2 @@
+#curl -u "apikey:{apikey}" "{url}/v3/classify?url=https://ibm.biz/BdzLPG&version=2018-03-19"
+curl -u "apikey:rZXy5a4_ImHy4uH6Ra7zooL4pRN27G_NMg4mSuw8tjL8" "https://api.us-south.visual-recognition.watson.cloud.ibm.com/instances/65aa8508-5efb-4457-9415-6f717d211dac/v3/classify?url=https://ibm.biz/BdzLPG&version=2018-03-19"
